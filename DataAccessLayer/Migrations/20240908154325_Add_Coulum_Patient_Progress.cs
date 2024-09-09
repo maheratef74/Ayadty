a66915bce10c4867e78a31dbc71459d59900035b
maheratef74 <maheratef600@gmail.com>
2024-09-09 22:46:37 +0300

    Resolved merge conflicts

---
+++ DataAccessLayer/Migrations/20240908154325_Add_Coulum_Patient_Progress.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Coulum_Patient_Progress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PatientContact",
+                table: "Appointments");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PatientProgress",
+                table: "Appointments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PatientProgress",
+                table: "Appointments");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PatientContact",
+                table: "Appointments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
