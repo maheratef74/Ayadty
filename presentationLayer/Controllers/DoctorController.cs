48ec04bc09d1d00c51449e5979c8c54c73d51454
AhmedGaffer <ahmedgaffer510@gmail.com>
2024-09-09 02:45:13 +0200

    add the 6 page without translation and missed the button for updating the profile photo

---
+++ presentationLayer/Controllers/DoctorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace presentationLayer.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+    }
+}
