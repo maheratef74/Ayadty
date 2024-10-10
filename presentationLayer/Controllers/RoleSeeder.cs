ee8cade7aa8b8aefcb0503ab07d0a4ad82f10e72
Shahd Gamal <shahdg999@gmail.com>
2024-10-10 19:52:07 +0100

    new apdate

--- presentationLayer/Controllers/RoleSeeder.cs
+++ presentationLayer/Controllers/RoleSeeder.cs
@@ -61,10 +61,10 @@ public class RoleSeeder
 
         foreach (var roleName in roleNames)
         {
-           /* if (!await _roleManager.RoleExistsAsync(roleName))
+            if (!await _roleManager.RoleExistsAsync(roleName))
             {
                 await _roleManager.CreateAsync(new IdentityRole(roleName));
-            }*/
+            }
         }
     }
     
